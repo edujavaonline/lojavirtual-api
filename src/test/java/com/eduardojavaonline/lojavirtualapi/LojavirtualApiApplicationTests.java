d0616f6eee9515d3b49d40ba9b2241cbe580bd67
edujavaonline <edureissantos007@gmail.com>
2019-07-31 14:23:32 -0300

    Primeiro commit

---
+++ src/test/java/com/eduardojavaonline/lojavirtualapi/LojavirtualApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.eduardojavaonline.lojavirtualapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LojavirtualApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
